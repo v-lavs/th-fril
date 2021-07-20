e9c655c01fac46c3d34d217551655c8f93a1924d
v-lavs <lavskavanda@gmail.com>
2021-07-20 23:05:52 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/*
+* to include js file write: `//= include ./path-to-file`
+* */
+
+//= include ../../node_modules/jquery/dist/jquery.js
+
+
+// CUSTOM SCRIPTS
+
+$(document).ready(function () {
+
+});
