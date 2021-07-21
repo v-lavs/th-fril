0001ef1bcd67662fb69ba8f2c1e80a0483558f09
v-lavs <lavskavanda@gmail.com>
2021-07-21 10:27:21 +0300

    commit 2

--- src/js/main.js
+++ src/js/main.js
@@ -8,5 +8,22 @@
 // CUSTOM SCRIPTS
 
 $(document).ready(function () {
+//SLIDER
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
 
 });
+
